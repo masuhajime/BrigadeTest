6c3875b4c73b4a0296ed5bf2267d59c590b86bdb
masuhajime <masuhajime@masuhajime-no-MacBook-Air.local>
2012-07-28 02:42:21 +0900

    ゲーム設定に必要な情報を定義

---
+++ config/game.js
@@ -0,0 +1,14 @@
+var ConfigGame = {
+    // 巡回クエストid
+    QUEST_ID: 101001,
+    // クエストを実行する最低LP
+    QUEST_LP_BORDER: 4,
+    // クエストを実行できない場合に待機する時間(ms)
+    QUEST_WAIT_TIME: 10*60*1000,
+    // アビリティーがこの数値以上なら自動売却
+    AUTO_SELL_NUM_ABILITY: 70,
+    // 武器がこの数値以上なら自動売却
+    AUTO_SELL_NUM_WEAPON: 70,
+    // 敵がこのHP以下ならば通常攻撃しかしない
+    ENEMY_HP_BORDER_FULL_ATTACK: 500000
+}
